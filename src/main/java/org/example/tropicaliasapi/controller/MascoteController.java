53f0310f5e91c3b1a0818fc26fa31b25a18dd9bf
nicolasruoco <nicolas.ruoco@germinare.org.br>
2024-09-16 08:48:19 -0300

    feat: creating the service and the controller for Mascote

--- src/main/java/org/example/tropicaliasapi/controller/MascoteController.java
+++ src/main/java/org/example/tropicaliasapi/controller/MascoteController.java
@@ -8,9 +8,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.tropicaliasapi.model.Estado;
-import org.example.tropicaliasapi.model.Follow;
+import org.example.tropicaliasapi.model.Mascote;
 import org.example.tropicaliasapi.service.EstadoService;
-import org.example.tropicaliasapi.service.FollowService;
+import org.example.tropicaliasapi.service.MascoteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,42 +18,42 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Follow")
+@Tag(name = "Mascote")
 @RestController
-@RequestMapping("/follow")
-public class FollowController {
-    FollowService followService;
+@RequestMapping("/mascote")
+public class MascoteController {
+    MascoteService mascoteService;
 
-    public FollowController(FollowService followService) {
-        this.followService = followService;
+    public MascoteController(MascoteService mascoteService) {
+        this.mascoteService = mascoteService;
     }
 
     @GetMapping
-    @Operation(summary = "Procurar todos as Interações entre usuários que se seguem")
+    @Operation(summary = "Procurar todos os mascotes dos usuários")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Follows retornadas com sucesso",
-                    content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Follow.class)))
+            @ApiResponse(responseCode = "200", description = "Mascotes retornadas com sucesso",
+                    content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Mascote.class)))
             ),
             @ApiResponse(responseCode = "500", description = "Erro interno do servidor", content = @Content)
     })
     public ResponseEntity<?> getAll() {
-        return new ResponseEntity<>(followService.getAll(), HttpStatus.OK);
+        return new ResponseEntity<>(mascoteService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    @Operation(summary = "Procurar follow pelo seu id")
+    @Operation(summary = "Procurar mascote pelo seu id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Follow retornada com sucesso",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Follow.class))
+            @ApiResponse(responseCode = "200", description = "Mascote retornada com sucesso",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Mascote.class))
             ),
-            @ApiResponse(responseCode = "404", description = "Follow não encontrada", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Mascote não encontrada", content = @Content),
             @ApiResponse(responseCode = "500", description = "Erro interno do servidor", content = @Content)
     })
     public ResponseEntity<?> getByID(@PathVariable("id") Long id) {
-        Follow follow = followService.getById(id);
-        if (follow == null) {
-            return new ResponseEntity<>("O follow não foi encontrado", HttpStatus.NOT_FOUND);
+        Mascote mascote = mascoteService.getById(id);
+        if (mascote == null) {
+            return new ResponseEntity<>("O mascote não foi encontrado", HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(follow, HttpStatus.OK);
+        return new ResponseEntity<>(mascote, HttpStatus.OK);
     }
 }
